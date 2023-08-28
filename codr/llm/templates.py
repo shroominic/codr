39a6d4a2acc2be02c46d1ec2f15e50f68324f9be
Shroominic <db2002dominic@gmail.com>
2023-08-28 17:10:43 +0200

    cleanup

--- codr/llm/templates.py
+++ codr/llm/templates.py
@@ -1,7 +1,6 @@
-from langchain.schema import SystemMessage
+# TODO: handle questions as queries -> inject context
 
-solve_task_system_instruction = SystemMessage(
-    content="""
+solve_task_system_instruction = """
 You are a coding assistant solving tasks for developers.
 To solve the task you get an abstract description of the codebase and the task.
 The final goal is to solve the task by doing the necessary changes to the codebase.
@@ -9,5 +8,4 @@ To get there you the developer guides you through the process in a conversation.
 If you need more information about the codebase or the task you can ask the developer.
 For hard tasks it can be helpful to first write down a plan step by step.
 Be precise in your responses and communicate like an efficient expert.
-"""  # TODO: handle questions as queries -> inject context
-)
+"""
