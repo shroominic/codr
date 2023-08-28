788b0e8922f994498661067cf0ed20344066409e
Shroominic <db2002dominic@gmail.com>
2023-08-28 17:10:13 +0200

    improve scripts

--- codr/llm/scripts.py
+++ codr/llm/scripts.py
@@ -1,72 +1,89 @@
 import asyncio
-from codr.codebase import CodeBase
-from codr.llm.schema import Task, PlannedFileChange, CreatedFile, ModifiedFile, DeletedFile, FileChange
-from codr.llm.chains import (
-    improve_task_description,
-    plan_file_changes,
-    create_file,
-    modify_file,
-)
+
+from codr.codebase.func import create_file, delete_file, fix_file_path, get_tree, modify_file, prepare_environment, bash
+from codr.llm.chains import create_file_prompt, improve_task_description, modify_file_prompt, plan_file_changes, summarize_task_to_name, check_result, generate_task
+from codr.llm.schema import CreatedFile, DeletedFile, FileChange, ModifiedFile, PlannedFileChange, Task
+from funcchain.utils import log
 
 
-async def solve_task(task: Task) -> None:
-    codebase = await CodeBase.load()
-    tree = await codebase.get_tree()
+async def solve_task(task_description: str) -> None:
+    task_name = summarize_task_to_name(task_description)
+    log("Task name:", task_name)
+    task = Task(
+        name=task_name,
+        description=task_description,
+    )
+    
+    tree = await get_tree()
 
     task.description = await improve_task_description(task, tree)
-    print("Improved task: ", task)
+    log("Improved task: ", task)
 
     changes = (
         await asyncio.gather(
-            compute_changes(task, codebase),
-            codebase.prepare_environment(task),
+            compute_changes(task),
+            prepare_environment(task),
         )
     )[0]
 
-    await apply_changes(codebase, changes)
+    await apply_changes(changes)
+    
+    # TODO: add auto_debug()
+
+    
+async def auto_debug() -> None:
+    result = await bash("./test.sh")
+
+    log("RESULT: ", result)
+
+    if await check_result(result):
+        return log("DEBUG SUCCESSFUL")
+
+    description = await generate_task(result)
+    log("TASK:", description)
+    await solve_task(description)
 
 
-async def compute_changes(task: Task, codebase: CodeBase) -> list[FileChange]:
-    planned_changes = await plan_file_changes(task, await codebase.get_tree())
-    print("Planned changes: ", planned_changes)
+async def compute_changes(task: Task) -> list[FileChange]:
+    planned_changes = await plan_file_changes(task, await get_tree())
+    log("Planned changes: ", planned_changes)
     return await asyncio.gather(
         *[
             generate_change(
                 task=task,
                 change=change,
-                codebase=codebase,
             )
             for change in planned_changes.changes
         ]
     )
 
 
-async def generate_change(task: Task, change: PlannedFileChange, codebase: CodeBase) -> FileChange:
+async def generate_change(task: Task, change: PlannedFileChange) -> FileChange:
     # TODO: collect relevant context
     # TODO: plan file changes precise based on context
-    print("Generating change: ", change)
-    change.relative_path = await codebase.fix_file_path(change.relative_path)
-    tree = await codebase.get_tree()
+    log("Generating change: ", change)
+    change.relative_path = await fix_file_path(change.relative_path)
+    tree = await get_tree()
     if change.method == "create":
-        return CreatedFile(relative_path=change.relative_path, content=(await create_file(change, tree)).code)
-    # elif change.method == "modify":
-    #     return ModifiedFile(relative_path=change.relative_path, changes=(await modify_file(task, tree, change)).changes)
+        return CreatedFile(relative_path=change.relative_path, content=(await create_file_prompt(change, tree)).code)
     elif change.method == "modify":
-        return ModifiedFile(relative_path=change.relative_path, content=(await modify_file(task, tree, change)).code)
+        return ModifiedFile(
+            relative_path=change.relative_path, content=(await modify_file_prompt(task, tree, change)).code
+        )
     elif change.method == "delete":
         return DeletedFile(relative_path=change.relative_path)
     else:
         raise ValueError(f"Invalid method: {change.method}")
 
 
-async def apply_changes(codebase: CodeBase, changes: list[FileChange]):
+async def apply_changes(changes: list[FileChange]):
     for change in changes:
-        print("Applying change: ", change)
+        log("Applying change: ", change)
         if isinstance(change, CreatedFile):
-            await codebase.create_file(change.relative_path, change.content)
+            await create_file(change.relative_path, change.content)
         elif isinstance(change, ModifiedFile):
-            await codebase.change_file(change.relative_path, change.content)
+            await modify_file(change.relative_path, change.content)
         elif isinstance(change, DeletedFile):
-            await codebase.delete_file(change.relative_path)
+            await delete_file(change.relative_path)
         else:
             raise ValueError(f"Invalid change: {change}")
