8735a3615e261789c79029e95eb82dae5d3474e5
Shroominic <db2002dominic@gmail.com>
2023-08-28 17:08:04 +0200

    config

---
+++ funcchain/config.py
@@ -0,0 +1,28 @@
+"""
+Funcchain Settings:
+Automatically loads environment variables from .env file
+"""
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+
+load_dotenv("./.env")
+
+
+class FuncchainSettings(BaseSettings):
+    # General
+    VERBOSE: bool = False
+    
+    # Prompt
+    MAX_TOKENS: int = 32768 - 8192
+    DEFAULT_SYSTEM_PROMPT: str = "You are an advanced programming assistant solving tasks for developers."
+    
+    # Model
+    OPENAI_API_KEY: str = ""
+    AZURE_API_KEY: str = ""
+    AZURE_API_BASE: str = ""
+    AZURE_DEPLOYMENT_NAME: str = ""
+    AZURE_API_VERSION: str = ""
+
+
+settings = FuncchainSettings()
