ba2c50793128defa487f8c149f65de5e6fe7dec7
Shroominic <db2002dominic@gmail.com>
2023-09-09 17:36:32 +0300

    🔧 Debugging enhancements in debug.py

--- codr/llm/chains/debug.py
+++ codr/llm/chains/debug.py
@@ -9,3 +9,16 @@ async def check_result(result: str) -> bool:
     Is the output healthy? Answer with "yes" or "no".
     """
     return await afuncchain()
+
+
+async def check_desired_output(result: str, goal: str) -> bool:
+    """
+    CONSOLE OUTPUT:
+    {result}
+
+    DESIRED OUTPUT (DESCRIPTION):
+    {goal}
+
+    Is the output what is desired? Answer with "yes" or "no".
+    """
+    return await afuncchain()
