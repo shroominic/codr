9597e8ffde9f160d40cddb0ad017f51119a81ad6
Shroominic <db2002dominic@gmail.com>
2023-09-16 17:23:43 +0200

    📝 Update commit.py script

--- codr/llm/chains/commit.py
+++ codr/llm/chains/commit.py
@@ -1,5 +1,5 @@
 from funcchain.parser import LambdaOutputParser
-from funcchain.shortcuts import afuncchain
+from funcchain.chain import achain, chain
 from funcchain.utils import raiser
 from emoji import is_emoji
 
@@ -16,14 +16,14 @@ async def write_commit_message(file_name: str, modifications: str) -> str:
     Start with a emoji and never answer with more than 5 words.
     Example: 🐛 Fix bug in foo.py
     """
-    return await afuncchain(
-        parser=LambdaOutputParser(
-            _parse=lambda t: t if len(t.split()) > 6 else summarize_commit_message(t),
-        )
+    return await achain(
+        # parser=LambdaOutputParser(
+        #     _parse=lambda t: t if len(t.split()) > 6 else summarize_commit_message(t),
+        # )  #  TODO: Fix this
     )
 
 
-async def summarize_commit_message(msg: str) -> str:
+def summarize_commit_message(msg: str) -> str:
     """
     COMMIT MESSAGE:
     {msg}
@@ -31,7 +31,7 @@ async def summarize_commit_message(msg: str) -> str:
     Summarize commit message. Answer with a compressed piece of knowledge.
     Start with a emoji and answer with less than 6 words.
     """
-    return await afuncchain(
+    return chain(
         parser=LambdaOutputParser(
             _parse=lambda t: t if is_emoji(t.split()[0]) else raiser(ValueError("Start with an emoji.")),
         )
