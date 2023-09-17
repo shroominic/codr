632a79a3e8c6687c55ceda120dc856496dde4782
Shroominic <db2002dominic@gmail.com>
2023-09-17 12:59:39 +0200

    📂 Update __init__.py in chains

--- codr/llm/chains/__init__.py
+++ codr/llm/chains/__init__.py
@@ -1,4 +1,4 @@
 from codr.llm.chains.commit import *
+from codr.llm.chains.debug import *
 from codr.llm.chains.files import *
 from codr.llm.chains.task import *
-from codr.llm.chains.debug import *
