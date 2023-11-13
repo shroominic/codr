fb02e5523daf1ac11d5c41ca2744923113022ee2
Shroominic <db2002dominic@gmail.com>
2023-11-13 12:00:02 +0100

    🔧 Refactor commit message logic

--- codr/llm/chains/commit.py
+++ codr/llm/chains/commit.py
@@ -1,37 +1,25 @@
-from emoji import is_emoji
-from funcchain.chain import achain, chain
+from funcchain import achain
 from funcchain.parser import LambdaOutputParser
 from funcchain.utils import raiser
 
 
 async def write_commit_message(file_name: str, modifications: str) -> str:
     """
-    FILE:
-    {file_name}
-
-    MODIFICATIONS:
-    {modifications}
-
     Write a tiny commit message for these file changes.
     Start with a emoji and never answer with more than 5 words.
     Example: 🐛 Fix bug in foo.py
     """
-    return await achain(
-        # parser=LambdaOutputParser(
-        #     _parse=(lambda t: t if len(t.split()) > 6 else summarize_commit_message(t)),
-        # )
-    )
+    return await achain()
 
 
-def summarize_commit_message(msg: str) -> str:
+async def summarize_commit_message(commit_message: str) -> str:
     """
-    COMMIT MESSAGE:
-    {msg}
-
     Summarize commit message. Answer with a compressed piece of knowledge.
     Start with a emoji and answer with less than 6 words.
     """
-    return chain(
+    from emoji import is_emoji
+
+    return await achain(
         parser=LambdaOutputParser(
             _parse=lambda t: t if is_emoji(t.split()[0]) else raiser(ValueError("Start with an emoji.")),
         )
