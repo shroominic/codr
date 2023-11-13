017358d41ba4c051e3b6a6ad9a707cffb73a389c
Shroominic <db2002dominic@gmail.com>
2023-11-13 12:00:02 +0100

    🧹 Refactor task.py imports

--- codr/llm/chains/task.py
+++ codr/llm/chains/task.py
@@ -1,18 +1,12 @@
-from funcchain.chain import achain
+from funcchain import achain
 from funcchain.parser import LambdaOutputParser
 
-from codr.codebase.tree import CodeBaseTree
-from codr.llm.schema import Task
+from ...codebase.tree import CodeBaseTree
+from ..schema import Task
 
 
-async def improve_task_description(task: Task, tree: CodeBaseTree) -> str:
+async def improve_task_description(task: Task, codebase_tree: CodeBaseTree) -> str:
     """
-    CODEBASE TREE:
-    {tree}
-
-    TASK DESCRIPTION:
-    {task}
-
     Improve task description based on context of codebase.
     Create a dense piece of knowledge giving understanding of task/goal.
     Write it for a programmer with only access to a part of codebase.
@@ -22,9 +16,6 @@ async def improve_task_description(task: Task, tree: CodeBaseTree) -> str:
 
 async def ask_additional_question(task: Task) -> str | None:
     """
-    TASK: {task.name}
-    {task.description}
-
     This is task you are supposed to solve.
     Evaluate if task is clear or if you have additional questions.
     Ask your question(s) or just answer 'Clear!'.
@@ -38,9 +29,6 @@ async def ask_additional_question(task: Task) -> str | None:
 
 async def summarize_task_to_name(task_description: str) -> str:
     """
-    TASK:
-    {task_description}
-
     Summarize task in less than 5 words to create a name for it.
     Only reply with this name.
     """
@@ -49,12 +37,6 @@ async def summarize_task_to_name(task_description: str) -> str:
 
 async def generate_task(console_output: str, desired_output: str) -> str:
     """
-    CONSOLE OUTPUT:
-    {console_output}
-
-    DESIRED OUTPUT:
-    {desired_output}
-
     Generate a task description to fix the error.
     """
     return await achain()
