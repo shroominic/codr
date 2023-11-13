bbc1b3ac959dee4c48276ea32574d697117935b4
Shroominic <db2002dominic@gmail.com>
2023-11-13 12:00:02 +0100

    🔧 Refactor import and parameters

--- codr/llm/chains/expert_answer.py
+++ codr/llm/chains/expert_answer.py
@@ -1,21 +1,18 @@
-from funcchain.chain import achain
-from langchain.pydantic_v1 import BaseModel, Field
+from funcchain import achain
+from langchain.pydantic_v1 import BaseModel
 
-from codr.codebase.tree import CodeBaseTree
+from ...codebase.tree import CodeBaseTree
 
 
 class RelevantFiles(BaseModel):
     relevant_files: list[str]
 
 
-async def get_relevant_files(question: str, tree: CodeBaseTree) -> RelevantFiles:
+async def get_relevant_files(
+    user_question: str,
+    codebase_tree: CodeBaseTree,
+) -> RelevantFiles:
     """
-    USER QUESTION:
-    {question}
-
-    CODEBASE TREE:
-    {tree}
-
     Which files are most relevant to answer the user question?
     Extract all relevant relative file paths from the codebase tree.
     Create a list containing relevant_files as strings. If no files are relevant, return an empty list.
@@ -23,16 +20,12 @@ async def get_relevant_files(question: str, tree: CodeBaseTree) -> RelevantFiles
     return await achain()
 
 
-async def codebase_answer(question: str, tree: CodeBaseTree, knowledge: list[str] = ["N/A"]) -> str:
+async def codebase_answer(
+    question: str,
+    codebase_tree: CodeBaseTree,
+    knowledge: list[str] = ["N/A"],
+) -> str:
     """
-    QUESTION: {question}
-
-    CODEBASE STRUCTURE:
-    {tree}
-
-    CONTEXT:
-    {knowledge}
-
     Answer the question based on the codebase and context.
     """
     return await achain()
