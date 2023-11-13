5c2734062c743ab60aa1e2943b378c2756c90a68
Shroominic <db2002dominic@gmail.com>
2023-11-13 12:00:02 +0100

    🔧 Refactor import statements

--- codr/llm/chains/files.py
+++ codr/llm/chains/files.py
@@ -1,15 +1,12 @@
-from funcchain.chain import achain
+from funcchain import achain
 from funcchain.parser import CodeBlock
 
-from codr.codebase.tree import CodeBaseTree
-from codr.llm.schema import PlannedFileChange, PlannedFileChanges, Task
+from ...codebase.tree import CodeBaseTree
+from ..schema import PlannedFileChange, PlannedFileChanges, Task
 
 
 async def summarize_file(content: str) -> str:
     """
-    FILE CONTENT:
-    {content}
-
     Summarize file content.
     Try to describe file in a way that a programmer can understand it without reading code.
     Make it fit in one line but don't be afraid to use multiple sentences.
@@ -18,41 +15,23 @@ async def summarize_file(content: str) -> str:
     return await achain()
 
 
-async def search_important_files(task: Task, tree: CodeBaseTree) -> list[str]:
+async def search_important_files(task: Task, codebase_tree: CodeBaseTree) -> list[str]:
     """
-    TASK:
-    {task}
-
-    CODEBASE TREE:
-    {tree}
-
     Which of these files are important to understand and solve task?
     """
     return await achain()
 
 
-async def fix_filename(file_name: str, tree: CodeBaseTree) -> str:
+async def fix_filename(relative_file_path: str, codebase_tree: CodeBaseTree) -> str:
     """
-    RELATIVE_FILE_PATH:
-    {file_name}
-
-    CODEBASE TREE:
-    {tree}
-
     Fix RELATIVE_FILE_PATH to match
     valid relative file path from CodeBaseTree.
     """
     return await achain()
 
 
-async def plan_file_changes(task: Task, tree: CodeBaseTree) -> PlannedFileChanges:
+async def plan_file_changes(task: Task, codebase_tree: CodeBaseTree) -> PlannedFileChanges:
     """
-    TASK:
-    {task}
-
-    CODEBASE TREE:
-    {tree}
-
     Which of these files from tree need to be modified to solve task?
     Answer with a list of file changes inside a JSON array.
     If you need to create a directory, use "mkdir" as method.
@@ -65,28 +44,16 @@ async def plan_file_changes(task: Task, tree: CodeBaseTree) -> PlannedFileChange
     return await achain()
 
 
-async def generate_file_change(file_name: str, abstract_plan: str, tree: CodeBaseTree) -> str:
+async def generate_file_change(file_name: str, abstract_plan: str, codebase_tree: CodeBaseTree) -> str:
     """
-    FILE:
-    {file_name}
-
-    CODEBASE TREE:
-    {tree}
-
-    ABSTRACT PLAN:
-    {abstract_plan}
-
     Generate a precise plan for file change.
     Answer with a compressed summary of knowledge describing what to change.
     """
     return await achain()
 
 
-async def create_file_prompt(change: PlannedFileChange, tree: CodeBaseTree) -> CodeBlock:
+async def create_file_prompt(change: PlannedFileChange, codebase_tree: CodeBaseTree) -> CodeBlock:
     """
-    CODEBASE TREE:
-    {tree}
-
     PLAN:
     {change_description}
 
@@ -102,14 +69,8 @@ async def create_file_prompt(change: PlannedFileChange, tree: CodeBaseTree) -> C
     )
 
 
-async def modify_file_prompt(task: Task, tree: CodeBaseTree, change: PlannedFileChange) -> CodeBlock:
+async def modify_file_prompt(main_task: Task, codebase_tree: CodeBaseTree, change: PlannedFileChange) -> CodeBlock:
     """
-    CODEBASE TREE:
-    {tree}
-
-    MAIN TASK:
-    {task}
-
     PLAN:
     {change_description}
 
