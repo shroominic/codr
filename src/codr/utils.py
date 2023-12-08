b5649aaf381b3c9b01b958a6d2cb2812b1ebfe79
Shroominic <db2002dominic@gmail.com>
2023-12-07 17:47:03 -0800

    ✨ Add kwargs to log function

--- src/codr/utils.py
+++ src/codr/utils.py
@@ -2,6 +2,6 @@ from typing import Any
 from funcchain import settings  # TODO: not use funcchain settings
 
 
-def log(*text: Any) -> None:
+def log(*text: Any, **kwargs: Any) -> None:
     if settings.DEBUG:
-        print(*text)
+        print(*text, **kwargs)
