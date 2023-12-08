3c6c9bb171f9098a09d7a165860fb6d4719f223c
Shroominic <db2002dominic@gmail.com>
2023-12-07 17:47:02 -0800

    🔍 Validate relevant file paths

--- src/codr/commands/ask.py
+++ src/codr/commands/ask.py
@@ -1,16 +1,26 @@
 from rich import print
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from funcchain import achain
 from ..codebase.func import (
     get_tree,
     read_file,
+    file_exists,
 )
 from ..codebase.tree import CodeBaseTree
 
 
 class RelevantFiles(BaseModel):
     relevant_files: list[str]
+
+    @field_validator("relevant_files")
+    def check_relevant_files(cls, v: list[str]) -> list[str]:
+        if len(v) > 7:
+            raise ValueError("Too many files")
+        for path in v:
+            if not file_exists(path):
+                raise ValueError(f"FilePath does not exist in Codebase: {path}")
+        return v
 
 
 async def get_relevant_files(
@@ -19,7 +29,7 @@ async def get_relevant_files(
 ) -> RelevantFiles:
     """
     Which files are most relevant to answer the user question?
-    Extract max 7 relevant relative file paths from the codebase tree.
+    return ONLY the relevant relative paths, produce the MINIMUM neccesary up to a MAXIMUM OF 7.
     Create a list containing relevant_files as strings.
     If no files are relevant, return an empty list.
     """
@@ -29,10 +39,10 @@ async def get_relevant_files(
 async def codebase_answer(
     question: str,
     codebase_tree: CodeBaseTree,
-    knowledge: list[str] = ["N/A"],
+    relevant_files: list[str] = ["N/A"],
 ) -> str:
     """
-    Answer the question based on the codebase and context.
+    Answer the question based on the codebase tree and relevant files.
     Format your answer in a way that is easy to read inside a terminal.
     You can utilize python rich format features.
     """
