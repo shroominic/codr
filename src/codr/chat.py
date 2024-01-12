8a0fd92fbe9708594e38ed86a483220e6b29c111
Shroominic <db2002dominic@gmail.com>
2024-01-12 20:21:39 +0100

    ✨ Add CLI command routing

--- src/codr/chat.py
+++ src/codr/chat.py
@@ -1,9 +1,52 @@
 import sys
+from enum import Enum
+
+from funcchain import chain
+from pydantic import BaseModel
+
+
+class CLICommand(str, Enum):
+    EXIT = "exit"
+    CHAT = "chat"
+    IMPLEMENT = "implement"
+    DEBUG = "debug"
+    COMMIT = "commit"
+    ASK = "ask"
+
+
+class CLISelector(BaseModel):
+    selection: CLICommand
+
+
+def router(user_request: str) -> CLISelector:
+    """
+    Select the appropriate cli command/action based on what the user wants.
+    """
+    return chain()
 
 
 def chat() -> None:
     cli_input = " ".join(sys.argv[1:])
-    print(cli_input)
+
+    route = router(cli_input)
+
+    print(route)
+
+    match route.selection:
+        case CLICommand.CHAT:
+            print("Chatting")
+        case CLICommand.DEBUG:
+            print("Debugging")
+        case CLICommand.COMMIT:
+            print("Commiting")
+        case CLICommand.ASK:
+            print("Asking")
+        case CLICommand.EXIT:
+            print("Exiting")
+        case CLICommand.IMPLEMENT:
+            print("Implementing")
+        case _:
+            print("Unknown command")
 
 
 if __name__ == "__main__":
