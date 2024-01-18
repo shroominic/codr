af190482fa5fc5915002bab4ae630a1ea1d6fd41
Shroominic <db2002dominic@gmail.com>
2024-01-18 02:17:36 +0100

    🔥 Remove debug condition logging

--- src/codr/utils.py
+++ src/codr/utils.py
@@ -1,9 +1,7 @@
 from typing import Any
 
-from funcchain import settings  # TODO: not use funcchain settings
 from rich import print
 
 
 def log(*text: Any, **kwargs: Any) -> None:
-    if settings.DEBUG:
-        print(*text, **kwargs)
+    print(*text, **kwargs)
