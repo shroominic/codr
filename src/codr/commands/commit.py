4932f7f4770e0b5a751eb23a0c5c7f8237917450
Shroominic <db2002dominic@gmail.com>
2024-03-04 11:46:51 +0700

    🔄 Update commit process logic

--- src/codr/commands/commit.py
+++ src/codr/commands/commit.py
@@ -19,43 +19,53 @@ async def write_commit_message(
 
 
 async def commit_changes(stage: bool, auto_push: bool) -> None:
+    print("Committing changes")
     if stage:
         await bash("git add .")
+
     git_status = (await bash("git status")).split("Changes not staged for commit:")[0]
+    print("bash completed")
     if "Changes to be committed" in git_status:
-        commits = await asyncio.gather(
-            *[process_change(change) for change in git_status.split("\n") if change.startswith("\t")]
-        )
-        for change, msg in commits:
+        commits = [
+            asyncio.create_task(process_change(change)) for change in git_status.split("\n") if change.startswith("\t")
+        ]
+        for task in asyncio.as_completed(commits):
+            change, msg = await task
+            print(f"[grey]{change}[/grey] > {msg}")
             await bash(f'git commit {change} -m "{msg}"')
-            print(change, ">", msg)
+
         if auto_push:
             await bash("git push")
 
 
 async def process_change(change: str) -> tuple[str, str]:
-    change_split = change.split()
-    if len(change_split) > 1:
+    print(change)
+    if len(change_split := change.split()) > 1:
         if change_split[0] == "modified:":
             file_change = change_split[1].strip()
             modifications = await bash(f"git diff --staged {file_change}")
             commit_msg = await write_commit_message(file_change, modifications)
             return file_change, commit_msg
+
         if change_split[0] == "new":
             file_change = change_split[2].strip()
             modifications = await bash(f"git diff --staged {file_change}")
             commit_msg = await write_commit_message(file_change, modifications)
             return file_change, commit_msg
+
         if change_split[0] == "deleted:":
             file_change = change_split[1].strip()
             commit_msg = await write_commit_message(file_change, "got deleted")
             return file_change, commit_msg
+
         if change_split[0] == "renamed:":
             file_change = change_split[3].strip()
             commit_msg = await write_commit_message(file_change, "got renamed")
             return file_change, commit_msg
+
         else:
             raise ValueError(f"Invalid change: {change}")
+
     elif len(change_split) == 1:
         file_change = change_split[0].strip()
         commit_msg = await write_commit_message(file_change, "")
