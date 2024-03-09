295c78e147ef7ad822caf2929c6ca5ab57101303
Shroominic <db2002dominic@gmail.com>
2024-03-09 18:46:34 +0700

    ✨ Add show_yes_no_select function in clientio.py

---
+++ src/shared/codebase/clientio.py
@@ -0,0 +1,9 @@
+# call client side ui actions and wait for response
+
+
+async def show_yes_no_select(question: str) -> bool:
+    """
+    Show cli select for yes or no
+    """
+    # todo implement
+    return True
