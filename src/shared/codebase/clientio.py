45f370dc203a22fa6a129c5a6417f1d1b92a37c8
Shroominic <db2002dominic@gmail.com>
2024-03-16 23:23:29 +0700

    💻 Update client-side UI interactions

--- src/shared/codebase/clientio.py
+++ src/shared/codebase/clientio.py
@@ -1,3 +1,6 @@
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+
 # call client side ui actions and wait for response
 
 
@@ -5,5 +8,13 @@ async def show_yes_no_select(question: str) -> bool:
     """
     Show cli select for yes or no
     """
-    # todo implement
-    return True
+    return (
+        await inquirer.select(  # type: ignore
+            message=question,
+            choices=[
+                Choice("y", "yes"),
+                Choice("n", "no"),
+            ],
+        ).execute_async()
+        == "y"
+    )
