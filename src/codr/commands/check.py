59da93be74a8c3b9ccab72d8fa2eb14078640fa2
Shroominic <db2002dominic@gmail.com>
2024-03-16 23:23:29 +0700

    ✨ Add new command for codebase checks

---
+++ src/codr/commands/check.py
@@ -0,0 +1,12 @@
+# GIT DIFF CHECK or CODEBASE CHECK
+
+# command to check current open changes for bugs
+# first group changes together that are related
+
+# quick check
+# do shallow quality check for small bugs or dumb mistakes
+
+# deep check
+# gather related files and tree for context
+# infer goal from changes
+# analyze if goal is reached
